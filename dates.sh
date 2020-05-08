64047505869a719e7f1e1f0eb78ea5668a09d80d
SHAIK SAJEETH <sajju.shaik143@gmail.com>
2020-05-08 14:24:44 +0530

    Update dates.sh

--- dates.sh
+++ dates.sh
@@ -1,11 +1,32 @@
-#!/bin/sh
-
-while read date
+# a million commits
+for Y in {2018..2019}
 do
-    fileName=`echo "$date" | tr " " "_"`
-    date="$date 14:00 2013 +0500"
-    echo "Creating file... $fileName"
-    touch "$fileName"
-    git add "$fileName"
-    git commit --date="$date" --author="sajju.shaik143@gmail.com" -m "$fileName"
-done <dates.txt
+  mkdir $Y
+  cd $Y
+  for M in {01..12}
+  do
+    mkdir $M
+    cd $M
+    for D in {01..31}
+    do
+      mkdir $D
+      cd $D
+      for i in {01..12}
+      do
+        echo "$i on $M/$D/$Y" > commit.md
+        export GIT_COMMITTER_DATE="$Y-$M-$D 12:$i:00"
+        export GIT_AUTHOR_DATE="$Y-$M-$D 12:$i:00"
+        git add commit.md -f
+        git commit --date="$Y-$M-$D 12:0$i:00" -m "$i on $M $D $Y"
+      done
+      cd ../
+    done
+    cd ../
+  done
+  cd ../
+done
+git push origin master
+git rm -rf 20**
+git rm -rf 19**
+git commit -am "cleanup"
+git push origin master
